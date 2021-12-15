fc99532746b69d4920173ad1b45262e296bc3a95
Urosh Trifunovic <nediarovic@gmail.com>
2021-12-15 08:53:52 +0100

    Add integration tests for user

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -3,15 +3,22 @@ import createServer from "../utils/server";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { createProduct } from "../service/product.service";
+import { signJWT } from "../utils/jwt.utils";
 
 const app = createServer();
 const userId = new mongoose.Types.ObjectId().toString();
 export const productPayload = {
     user: userId,
     title: "Cannon E0S 1500D DSLR Camera with 18-55mm Lens",
-    description: "Lorem Ipsum bla bla...",
+    description: "Designed for first-time DSLR owners who want impressive results out of the box and capture those magic moments on camera!",
     price: 999.81,
     image: "https://i.imgur.com/QlRphfQ.jpg",
+}
+
+export const userPayload = {
+    _id: userId,
+    email: "jane.doe@example.com",
+    name: "Jane Doe"
 }
 
 
@@ -44,6 +51,34 @@ describe("product", () => {
     });
 
     describe("create product route", () => {
-        
+        describe("given the user is not logged in", () => {
+            it("should return a 403", async () => {
+                const { statusCode } = await supertest(app).post("/api/products");
+                expect(statusCode).toBe(403);
+            });
+        });
+        describe("given the user is logged in", () => {
+            it("should return a 200 and create a product", async () => {
+                const jwt = signJWT(userPayload, "accessTokenPrivateKey");
+                const { statusCode, body } = await supertest(app)
+                    .post("/api/products")
+                    .set("Authorization", `Bearer ${jwt}`)
+                    .send(productPayload);
+
+                expect(statusCode).toBe(200);
+                expect(body).toStrictEqual({
+                    __v: 0,
+                    _id: expect.any(String),
+                    createdAt: expect.any(String),
+                    description: "Designed for first-time DSLR owners who want impressive results out of the box and capture those magic moments on camera!",
+                    image: "https://i.imgur.com/QlRphfQ.jpg",
+                    price: 999.81,
+                    productId: expect.any(String),
+                    title: "Cannon E0S 1500D DSLR Camera with 18-55mm Lens",
+                    updatedAt: expect.any(String),
+                    user: expect.any(String)
+                    });
+            });
+        });
     });
 });
---
+++ src/__tests__/user.test.ts
@@ -0,0 +1,22 @@
+describe("user", () => {
+    describe("user registration", () => {
+        describe("given the username and password are valid", () => {
+            it("should return the user payload", () => {
+
+            });
+        });
+        describe("given the passwords do not match", () => {
+            it("should return 400", () => {
+
+            });
+        });
+        describe("given an error happens", () => {
+            it("should be handled accordingly", () => {
+
+            });
+        });
+    });
+
+    // create a user session
+        // a user can login with a valid email and password
+});
